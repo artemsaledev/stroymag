d6d6cebe64e9c969fcee4fb1dd79b2c44dc704eb
Artem Yavdokimenko <timmaddy01@gmail.com>
2019-02-12 02:56:04 +0200

    Adds header layout, also fonts, navigation blocks: a sandwich and a basket

--- dev/static/js/main.js
+++ dev/static/js/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
     svg4everybody({});
+
+    var sandwich = function () {
+	$(document).on('click','sandwich', function (){
+	$(this).toggleClass('sandwich--active');
+		});
+	};
+	sandwich();
 });
